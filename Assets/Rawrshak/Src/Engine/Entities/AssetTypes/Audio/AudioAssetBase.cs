280e4a3006e9e8d261c37c188186b7d93f5a0033
Christian Sumido <gcbsumid@gmail.com>
2022-01-02 20:01:19 -0800

    Minor Fixes (#7)
    * Cleaned up Warnings
    
    * Wallet not in subgraph bug fix
    
    * Make AssetBundles temporary, unload immediately. Add Reset to Rawrshak Wallet manager

--- Assets/Rawrshak/Src/Engine/Entities/AssetTypes/Audio/AudioAssetBase.cs
+++ Assets/Rawrshak/Src/Engine/Entities/AssetTypes/Audio/AudioAssetBase.cs
@@ -23,22 +23,14 @@ namespace Rawrshak
             Aiff
         }
 
+        // todo: add Compression
+
         private static string Engine = "unity";
 
         protected AudioMetadataBase metadata;
         protected Dictionary<ContentTypes, AudioProperties> audioData;
         protected ContentTypes currentContentType;
         protected AudioClip currentAudioClip;
-        protected AssetBundle currentAssetBundle;
-
-        void OnDisabled()
-        {
-            // Unload completely before disabling object
-            if (currentAssetBundle)
-            {
-                currentAssetBundle.Unload(true);
-            }
-        }
 
         public override void Init(PublicAssetMetadataBase baseMetadata)
         {
@@ -81,17 +73,11 @@ namespace Rawrshak
                 return null;
             }
 
-            AssetBundle assetBundle = currentAssetBundle;
-            if (currentContentType == ContentTypes.Invalid || data.uri != audioData[type].uri)
+            AssetBundle assetBundle = await Downloader.DownloadAssetBundle(data.uri);
+            if (assetBundle == null)
             {
-                // Download the assetbundle
-                // Debug.Log("****** Audio URI: " + data.uri);
-                assetBundle = await Downloader.DownloadAssetBundle(data.uri);
-                if (assetBundle == null)
-                {
-                    Debug.LogError("AssetBundle not found");
-                    return null;
-                }
+                Debug.LogError("AssetBundle not found");
+                return null;
             }
 
             // Debug.Log("****** Filename: " + data.name);
@@ -110,13 +96,7 @@ namespace Rawrshak
                 return null;
             }
 
-            // If a new AssetBundle was loaded that's different from the current bundle
-            if (currentAssetBundle && assetBundle != currentAssetBundle)
-            {
-                currentAssetBundle.Unload(false);
-            }
-
-            currentAssetBundle = assetBundle;
+            assetBundle.Unload(false);
             currentAudioClip = audioClip;
             currentContentType = type;
             return currentAudioClip;
--- Assets/Rawrshak/Src/Wallet/Wallet.cs
+++ Assets/Rawrshak/Src/Wallet/Wallet.cs
@@ -34,10 +34,24 @@ namespace Rawrshak
         {
             GetAccountInfo.ReturnData responseData = await GetAccountInfo.Fetch(address);
 
+            if (responseData.data.account.address == null)
+            {
+                Debug.Log("Info: Unused Wallet");
+                return;
+            }
+
             walletAddress = responseData.data.account.address;
             mintCount = BigInteger.Parse(responseData.data.account.mintCount);
             burnCount = BigInteger.Parse(responseData.data.account.burnCount);
             uniqueAssetCount = BigInteger.Parse(responseData.data.account.uniqueAssetCount);
+        }
+
+        public void Reset()
+        {
+            walletAddress = String.Empty;
+            mintCount = 0;
+            burnCount = 0;
+            uniqueAssetCount = 0;
         }
 
         public async Task<List<KeyValuePair<Asset, int>>> GetAllAssetsInWallet(int amount, string lastId)
@@ -45,6 +59,12 @@ namespace Rawrshak
             List<KeyValuePair<Asset, int>> assets = new List<KeyValuePair<Asset, int>>();
 
             GetAssetsInWallet.ReturnData responseData = await GetAssetsInWallet.Fetch(walletAddress, amount, lastId);
+
+            if (responseData.data.account.assetBalances == null)
+            {
+                Debug.Log("Info: No Rawrshak Assets in Wallet");
+                return assets;
+            }
 
             foreach (var balanceData in responseData.data.account.assetBalances)
             {
@@ -64,6 +84,11 @@ namespace Rawrshak
 
             GetWalletAssetsInContent.ReturnData responseData = await GetWalletAssetsInContent.Fetch(walletAddress, contractAddress, amount, lastId);
 
+            if (responseData.data.account.assetBalances == null)
+            {
+                Debug.Log("Info: No Rawrshak Assets in Wallet");
+                return assets;
+            }
 
             foreach (var balanceData in responseData.data.account.assetBalances)
             {
@@ -82,6 +107,12 @@ namespace Rawrshak
             List<KeyValuePair<Asset, int>> assets = new List<KeyValuePair<Asset, int>>();
             GetWalletAssetsOfType.ReturnData responseData = await GetWalletAssetsOfType.Fetch(walletAddress, type, amount, lastId);
 
+            if (responseData.data.account.assetBalances == null)
+            {
+                Debug.Log("Info: No Rawrshak Assets in Wallet");
+                return assets;
+            }
+
             foreach (var balanceData in responseData.data.account.assetBalances)
             {
                 Asset asset = ScriptableObject.CreateInstance<Asset>();
@@ -99,6 +130,12 @@ namespace Rawrshak
             List<KeyValuePair<Asset, int>> assets = new List<KeyValuePair<Asset, int>>();
 
             GetWalletAssetsOfSubtype.ReturnData responseData = await GetWalletAssetsOfSubtype.Fetch(walletAddress, subtype, amount, lastId);
+
+            if (responseData.data.account.assetBalances == null)
+            {
+                Debug.Log("Info: No Rawrshak Assets in Wallet");
+                return assets;
+            }
 
             foreach (var balanceData in responseData.data.account.assetBalances)
             {
